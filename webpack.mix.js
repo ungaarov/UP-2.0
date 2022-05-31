3c68efb8345f2b3be1029ad35fde9e5bdd2a76ce
Пользователь <Aidyn888>
2022-05-31 19:21:14 +0600

    Little changes

--- webpack.mix.js
+++
@@ -1,5 +0,0 @@
-const mix = require('laravel-mix');
-
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/home.scss', 'public/css/');
-mix.copyDirectory('resources/image', 'public/image');
